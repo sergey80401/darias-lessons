bdef6d65fd31504e586ec30e2446ad88559a939e
skryuchkov <skryuchkov@gemsdev.com>
2024-03-21 13:30:01 +0600

    Lesson 1 has been created

---
+++ Lesson1/test.js
@@ -0,0 +1,19 @@
+const isPalindrome = require("./index.js");
+
+console.log(isPalindrome(""))
+
+test('Test 1', () => {
+    expect(isPalindrome("Шалаш")).toBe(true)
+})
+
+test('Test 2', () => {
+    expect(isPalindrome("лепс спел")).toBe(true)
+})
+
+test('Test 3', () => {
+    expect(isPalindrome("89т4309молпдки2п")).toBe(false)
+})
+
+test('Test 4', () => {
+    expect(isPalindrome("     ")).toBe(true)
+})
