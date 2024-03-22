5a3e7be980be7f7b80c7862aa1470a5808fd09f0
DKryuchkova <DKryuchkova@AIRLAN2.WELTALL.local>
2024-03-22 17:16:04 +0600

    РАБотает isPalindrome

--- Lesson1/index.js
+++ Lesson1/index.js
@@ -1,9 +1,18 @@
 // Написать функцию isPalindrome, которая принимает в качестве параметра строку и возвращет true, если эта строка является палиндромом и false, если нет.
 
 const isPalindrome = (str) => {
-  // ТУТ
-  // ТВОЙ
-  // КОД
+  str=str.toLowerCase();
+  j=true;
+  for (let i=0; i<=Math.floor((str.length-1)/2);i++){
+    if(str[i]==str[str.length-1-i]){
+      continue;
+    }
+    else{
+      j=false;
+    }
+  }
+  return j;
 }
+console.log(isPalindrome("123321"));
 
 module.exports = isPalindrome
