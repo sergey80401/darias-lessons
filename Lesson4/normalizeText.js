ed4269154646897bd7dc344d2c942f7bd73f12ab
skryuchkov <skryuchkov@gemsdev.com>
2024-04-01 12:56:45 +0600

    Lesson4 has been added

---
+++ Lesson4/normalizeText.js
@@ -0,0 +1,9 @@
+
+
+const normalizeText = (str) => {
+  // ТУТ
+  // ТВОЙ
+  // КОД
+}
+
+module.exports = normalizeText
---
+++ Lesson4/test.js
@@ -0,0 +1,21 @@
+const normalizeText = require("./normalizeText.js");
+
+test("Test 1", () => {
+  // ТУТ
+  // ТВОЙ
+  // КОД
+});
+
+test("Test 2", () => {
+  // ТУТ
+  // ТВОЙ
+  // КОД
+});
+
+test("Test 3", () => {
+  // ТУТ
+  // ТВОЙ
+  // КОД
+});
+
+
