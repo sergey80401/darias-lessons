f45e089bdfe25dd43b2e86dd851b0a1d5468a80a
Minibon <dariakryuchkov@gmail.com>
2024-04-01 20:41:21 +0600

    Доделано

--- Lesson2/largestCommonSubstring.js
+++ Lesson2/largestCommonSubstring.js
@@ -1,38 +1,47 @@
 // Написать функцию largestCommonSubstring, которая принимает в качестве параметров две строки: str1 и str2. И Возвращает наибольшую общую подстроку, УЧИТЫВАЯ ПРОБЕЛЫ
-// Пример: 
+// Пример:
 // str1 = "Даше нравится кодить на JavaScript"
 // str2 = "Программистам на Java нравится пить кофе"
 //
 // P.S. В str1 и str2 много общих подстрок, например " на Java", "ить", и даже " " будет считаться общей подстрокой, так как обе строки содержат пробелы. Но надо найти наибольшую
-// 
+//
 // Результат выполнения функции: " нравится "
 
 //str1 = "ABC_A"
 //str2 = "CAB_BC_A"
 const largestCommonSubstring = (str1, str2) => {
   var array = [];
-  var substr="";
+  var substr = "";
   //String largestSubstring;
-  for (i=0; i<str1.length; i++){
-      for (j=0; j<str2.length; j++){
-          if (str1[i]==str2[j]){
-              var substr = substr+str1[i];
-              //array.push(substr);
-              i++;
-          }else{
-              array.push(substr);
-              substr="";
-          }
+  for (i = 0; i < str1.length; i++) {
+    var offset = 0;
+    for (j = 0; j < str2.length; j++) {
+      if (str1[i+offset] == str2[j]) {
+        var substr = substr + str1[i+offset];
+        //array.push(substr);
+        offset++;
+      } else {
+        if (substr)
+            array.push(substr);
+        substr = "";
+        offset = 0
       }
+    }
   }
   array.push(substr);
-  var largestSubstring=array[0];
-  for (k=1; k<array.length; k++){
-      if (largestSubstring.length<array[k].length){
-      largestSubstring=array[k];
-      }
+  console.log(array.length);
+  var largestSubstring = array[0];
+  for (k = 1; k < array.length; k++) {
+    if (largestSubstring.length < array[k].length) {
+      largestSubstring = array[k];
+    }
   }
   return largestSubstring;
-}
-
-module.exports = largestCommonSubstring
+};
+console.log(
+  largestCommonSubstring(
+    "Даше нравится кодить на JavaScript",
+    "Программистам на Java нравится пить кофе"
+  )
+);
+module.exports = largestCommonSubstring;
