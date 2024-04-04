6e25c4b94b7fb3b13b11b65d0c6139656ef6ddca
skryuchkov <skryuchkov@gemsdev.com>
2024-04-04 11:11:03 +0600

    templates

---
+++ Lesson5/getRandomNumber.js
@@ -0,0 +1,10 @@
+// Реализуй функцию getRandomNumber, которая возвращает случайное значение в пределах от min до max (включительно) 
+
+const getRandomNumber = (min, max) => {
+  // ТУТ
+  // ТВОЙ
+  // КОД
+  return 0;
+}
+
+module.exports = getRandomNumber
