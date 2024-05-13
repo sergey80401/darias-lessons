337ec2e5d5ac00a3679455ccd56916bd7a80ac49
Minibon <dariakryuchkov@gmail.com>
2024-05-13 14:46:17 +0600

    постоянные ошибки

--- 0/Lesson6/index.ts
+++ 0/Lesson6/index.ts
@@ -1,17 +1,39 @@
 class Student {
-// Надо реализовать класс Student, объекты которого смогут хранить
-// информацию об ученике и о том, какие у него оценки и по каким предметам
+    name: string;
+    marks: Mark[];
+    constructor(studentName: string, studentMarks: Mark[]){
+        this.name = studentName;
+        this.marks = studentMarks;
+    }
 }
 
 class Mark {
-// Надо реализовать класс отметки, объекты которого смогут хранить информацию о том,
-//  какая оценка по той или иной дисциплине
+subject: string;
+value: number;
+constructor(studentSubject: string, studentValue: number){
+    this.subject = studentSubject;
+    this.value = studentValue;
 }
 
-// В идеале конечно раскидать все эти классы по разным файликам
+public get _value(): number {
+    return this.value;
+}
+public set _value(n: number) {
+    if(n < 1 || n > 5){
+        console.log("Нет таких оценок!");
+    }
+    else{
+        this.value = n;
+    }
+}
+}
+
+const sergey = new Student("Sergey");
+sergey.marks.push(new Mark("programming", 5));
 
 function getInfo(students: Student[]): void {
 // Надо реализовать функцию, которая в качестве параметров принимает массив объектов типа студент
 //  которая выводила бы в консоль информацию о об успеваемости студентов,
 // их оценки, средние баллы и средние баллы по дисциплинам
 }
+console.log(sergey.marks);
